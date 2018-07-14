1057e893827870a9335b0f6c9eee715aa813381f
tonywu70 <tony.wenyong@gmail.com>
2018-07-14 16:02:19 +0800

    Update install_beegfs.sh

--- BeeGFS/beegfsscripts/install_beegfs.sh
+++ BeeGFS/beegfsscripts/install_beegfs.sh
@@ -272,7 +272,8 @@ tune_storage()
 	sed -i 's/^tuneFileReadAheadTriggerSize.*/tuneFileReadAheadTriggerSize = 2m/g' /etc/beegfs/beegfs-storage.conf
 	sed -i 's/^tuneFileReadSize.*/tuneFileReadSize = 256k/g' /etc/beegfs/beegfs-storage.conf
 	sed -i 's/^tuneFileWriteSize.*/tuneFileWriteSize = 256k/g' /etc/beegfs/beegfs-storage.conf
-	sed -i 's/^tuneWorkerBufSize.*/tuneWorkerBufSize = 16m/g' /etc/beegfs/beegfs-storage.conf	
+	sed -i 's/^tuneWorkerBufSize.*/tuneWorkerBufSize = 16m/g' /etc/beegfs/beegfs-storage.conf
+	sed -i 's/^storeAllowFirstRunInit.*/storeAllowFirstRunInit = false/g' /etc/beegfs/beegfs-storage.conf
 }
 
 tune_meta()
@@ -284,6 +285,8 @@ tune_meta()
 
 	sed -i 's/^connMaxInternodeNum.*/connMaxInternodeNum = 800/g' /etc/beegfs/beegfs-meta.conf
 	sed -i 's/^tuneNumWorkers.*/tuneNumWorkers = 128/g' /etc/beegfs/beegfs-meta.conf
+	sed -i 's/^storeAllowFirstRunInit.*/storeAllowFirstRunInit = false/g' /etc/beegfs/beegfs-meta.conf
+
 }
 
 tune_tcp()
